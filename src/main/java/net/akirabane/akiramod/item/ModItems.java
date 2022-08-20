c98e061e4cb651a7e5cd84c8b1c34ebcffdaf763
Joshua BENON <joshuabenon988@yahoo.fr>
2022-08-20 17:48:35 +0200

    added custom mythril sword + craft

--- src/main/java/net/akirabane/akiramod/item/ModItems.java
+++ src/main/java/net/akirabane/akiramod/item/ModItems.java
@@ -3,8 +3,7 @@ package net.akirabane.akiramod.item;
 import net.akirabane.akiramod.AkiraMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -35,6 +34,9 @@ public class ModItems {
             new ArmorItem(ModArmorMaterials.MYTHRIL, EquipmentSlot.FEET,
                     new FabricItemSettings().group(ModItemGroup.MYTHRIL)));
 
+    public static final Item MYTHRIL_SWORD = registerItem("mythril_sword",
+            new SwordItem(ToolMaterials.NETHERITE, 15, 7f,
+                    new FabricItemSettings().group(ModItemGroup.MYTHRIL).maxCount(1)));
 
 
     private static Item registerItem(String name, Item item) {
